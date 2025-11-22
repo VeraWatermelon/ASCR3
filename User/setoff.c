f62b70fcaab8fc3084b94d211b89018311f6d13b
VeraWatermelon <19730032635@163.com>
2025-11-22 20:56:53 +0800

    添加编码器测量功能

--- User/setoff.c
+++ User/setoff.c
@@ -2,10 +2,14 @@
 #include "Motor.h"
 #include "IRSensor.h"
 #include "Timer.h"
+#include "Encoder.h"
+#include "Control.h"
 void Setoff(void)
 {
 	Motor_Init();
 	Timer1_Init();
+	Encoder1_Init();
+	Encoder2_Init();
 	IRSensor_Init();
 	while(1)
 	{
@@ -17,7 +21,7 @@ void TIM1_UP_IRQHandler(void)
 {
 	if (TIM_GetITStatus(TIM1, TIM_IT_Update) == SET)
 	{
-		
+		control();//执行比例再调调
 		
 		TIM_ClearITPendingBit(TIM1, TIM_IT_Update);
 	
