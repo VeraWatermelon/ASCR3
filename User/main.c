b243758904d3f57c9fb6f928231bef34d36e15b3
VeraWatermelon <19730032635@163.com>
2025-11-22 10:36:45 +0800

    添加按键

--- User/main.c
+++ User/main.c
@@ -1,13 +1,17 @@
 #include "stm32f10x.h"                  // Device header
 #include "Delay.h"
 #include "OLED.h"
+#include "Key.h"
 
 int main(void)
 {
 	OLED_Init();
-	
+	Key_Init();
 	while (1)
 	{
-		
+		if(Key_GetNum()==1)
+		{
+			
+		}
 	}
 }
