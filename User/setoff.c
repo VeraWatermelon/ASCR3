2294dc7e83f87d80ebb9100081a5ff77c2ad9834
VeraWatermelon <19730032635@163.com>
2025-11-22 20:40:49 +0800

    添加定时中断

--- User/setoff.c
+++ User/setoff.c
@@ -1,12 +1,26 @@
 #include "stm32f10x.h"                  // Device header
 #include "Motor.h"
 #include "IRSensor.h"
+#include "Timer.h"
 void Setoff(void)
 {
-	IRSensor_Init();
 	Motor_Init();
+	Timer1_Init();
+	IRSensor_Init();
 	while(1)
 	{
 	
 	}
+}
+
+void TIM1_UP_IRQHandler(void)
+{
+	if (TIM_GetITStatus(TIM1, TIM_IT_Update) == SET)
+	{
+		
+		
+		TIM_ClearITPendingBit(TIM1, TIM_IT_Update);
+	
+	}
+
 }
