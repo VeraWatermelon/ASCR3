6003fd615df1e0e9f0aa4966033a13a93d6eb527
VeraWatermelon <19730032635@163.com>
2025-11-23 10:18:46 +0800

    添加状态机，直行时才pid同步

--- User/Control.c
+++ User/Control.c
@@ -7,41 +7,100 @@ void control(void)//先随便写个数，之后再慢慢调/如果你电机写�
 {
 	uint8_t state;
 	state=IRSensorL1_Get()|IRSensorL2_Get()|IRSensorR2_Get()|IRSensorR1_Get();
-	switch (state)
+	if(S==1)
 	{
-		case 0:		//1111或0000直走
-			MotorA_SetSpeed(10);
-			MotorB_SetSpeed(10);
-		break;
-		case 15:
-			MotorA_SetSpeed(10);
-			MotorB_SetSpeed(10);
-		break;
-		case 4: //0100左转
-			MotorA_SetSpeed(20);
-			MotorB_SetSpeed(30);
-		break;
-		case 2://0010右转
-			MotorA_SetSpeed(30);
-			MotorB_SetSpeed(20);
-			break;
-		case 8://1000猛左转
-			MotorA_SetSpeed(10);
-			MotorB_SetSpeed(20);
-			break;
-		case 1://0001猛右转
-			MotorA_SetSpeed(20);
-			MotorB_SetSpeed(10);
-			break;
-		case 12://1100猛猛左转
-			MotorA_SetSpeed(0);
-			MotorB_SetSpeed(40);
-		break;
-		case 3://0011猛猛右转
-			MotorA_SetSpeed(40);
-			MotorB_SetSpeed(0);
-		break;
-		default:
-		break;
+			switch (state)
+			{
+				case 0:		//1111或0000直走
+					MotorA_SetSpeed(10);
+					MotorB_SetSpeed(10);
+				break;
+				case 15:
+					MotorA_SetSpeed(10);
+					MotorB_SetSpeed(10);
+				break;
+				case 4: //0100左转
+					MotorA_SetSpeed(20);
+					MotorB_SetSpeed(30);
+					S=2;
+				break;
+				case 2://0010右转
+					MotorA_SetSpeed(30);
+					MotorB_SetSpeed(20);
+					S=2;
+					break;
+				case 8://1000猛左转
+					MotorA_SetSpeed(10);
+					MotorB_SetSpeed(20);
+					S=2;
+					break;
+				case 1://0001猛右转
+					MotorA_SetSpeed(20);
+					MotorB_SetSpeed(10);
+					S=2;
+					break;
+				case 12://1100猛猛左转
+					MotorA_SetSpeed(0);
+					MotorB_SetSpeed(40);
+					S=2;
+				break;
+				case 3://0011猛猛右转
+					MotorA_SetSpeed(40);
+					MotorB_SetSpeed(0);
+					S=2;
+				break;
+				default:
+				break;
+			}
 	}
+	else
+	{
+		switch (state)
+			{
+				case 0:		//1111或0000直走
+					TargetB=0;//进入直行，实际位置清零
+					ActualB1=0;//这个和电机驱动的前后位置有说法吗，还有pid的开启
+					MotorA_SetSpeed(10);
+					MotorB_SetSpeed(10);
+					S=1;
+				break;
+				case 15:
+					TargetB=0;//进入直行，实际位置清零
+					ActualB1=0;
+					MotorA_SetSpeed(10);
+					MotorB_SetSpeed(10);
+					S=1;
+				break;
+				case 4: //0100左转
+					MotorA_SetSpeed(20);
+					MotorB_SetSpeed(30);
+				break;
+				case 2://0010右转
+					MotorA_SetSpeed(30);
+					MotorB_SetSpeed(20);
+					break;
+				case 8://1000猛左转
+					MotorA_SetSpeed(10);
+					MotorB_SetSpeed(20);
+					break;
+				case 1://0001猛右转
+					MotorA_SetSpeed(20);
+					MotorB_SetSpeed(10);
+					break;
+				case 12://1100猛猛左转
+					MotorA_SetSpeed(0);
+					MotorB_SetSpeed(40);
+				break;
+				case 3://0011猛猛右转
+					MotorA_SetSpeed(40);
+					MotorB_SetSpeed(0);
+				break;
+				default:
+				break;
+			}
+	
+	}		
+		
+		
+		
 }
--- User/setoff.c
+++ User/setoff.c
@@ -4,11 +4,11 @@
 #include "Timer.h"
 #include "Encoder.h"
 #include "Control.h"
-float ActualB1,ActualB2,TargetB,OutB;
+float ActualB1,TargetB,OutB;
 float KpB,KiB,KdB;
 float ErrorB0, ErrorB1, ErrorB2;
 uint8_t S;
-//还没调的：速度环pid,红外距离，pwm速度，pid与传感器比例暂时1：4，状态机还没写
+//还没调的：速度环pid,红外距离，pwm速度，pid与传感器比例暂时1：4
 void Setoff(void)
 {
 	Motor_Init();
@@ -16,6 +16,9 @@ void Setoff(void)
 	Encoder1_Init();
 	Encoder2_Init();
 	IRSensor_Init();
+	while(1)
+	{}
+		//怎么让他一直在这啊,这样会不会太费了...,好像有个神秘空函数。
 }
 
 void TIM1_UP_IRQHandler(void)//5ms一次
@@ -23,21 +26,22 @@ void TIM1_UP_IRQHandler(void)//5ms一次
 	if (TIM_GetITStatus(TIM1, TIM_IT_Update) == SET)
 	{
 				control();//执行比例再调调目前想做成：传感器20ms，pid5ms可以在++，
-				TargetB += Encoder2_Get();//状态机清零还没写 
-				ActualB1 += Encoder1_Get();//直走才用pid
-				ErrorB2 = ErrorB1;
-				ErrorB1 = ErrorB0;
-				ErrorB0 = TargetB - ActualB1;
-				
-				OutB += KpB * (ErrorB0 - ErrorB1) + KiB * ErrorB0
-						+ KdB * (ErrorB0 - 2 * ErrorB1 + ErrorB2);
-				
-				if (OutB > 100) {OutB = 100;}
-				if (OutB < -100) {OutB = -100;}
-				MotorA_SetSpeed(OutB);
-		
+				if(S==1)//直走才用pid
+				{
+					TargetB += Encoder2_Get();
+					ActualB1 += Encoder1_Get();
+					ErrorB2 = ErrorB1;
+					ErrorB1 = ErrorB0;
+					ErrorB0 = TargetB - ActualB1;
+					
+					OutB += KpB * (ErrorB0 - ErrorB1) + KiB * ErrorB0
+							+ KdB * (ErrorB0 - 2 * ErrorB1 + ErrorB2);
+					
+					if (OutB > 100) {OutB = 100;}
+					if (OutB < -100) {OutB = -100;}
+					MotorA_SetSpeed(OutB);
+				}
 		TIM_ClearITPendingBit(TIM1, TIM_IT_Update);
-	
 	}
 
 }
